26208529025bd6bc5fff627739cde0187ca0db88
istareatscreens <schar138@uottawa.ca>
2020-03-12 22:06:46 -0400

    Adjusted businessHour testing

--- test/testObjbH.ts
+++ test/testObjbH.ts
@@ -4,26 +4,22 @@ let jsondata = require("./assets/hours_test_template.json");
 export let dateTestOne = new Date();
 const { Day: days, Holidays: holidays } = jsondata;
 
-function createArrayOfDatesShifted() {
-  let dates = [];
-  for (let i = -1; i < 6; i++) {
-    dates.push(
-      new Date(
-        new Date(dateTestOne).getFullYear(),
-        new Date(dateTestOne).getMonth(),
-        new Date(dateTestOne).getDate() + i,
-        new Date(dateTestOne).getHours()
-      )
-    );
-  }
-  return dates;
-}
+// function createArrayOfDatesShifted() {
+//   let dates = [];
+//   for (let i = -1; i < 6; i++) {
+//     dates.push(
+//       new Date(
+//       )
+//     );
+//   }
+//   return dates;
+// }
 
 function getProperties(dates, jsondata) {
   const { Day: days, Holidays: holidays } = jsondata;
   let properties = [];
   for (let i = 0; i < 7; i++) {
-    let day = Day.init(i, days, dates[0], holidays);
+    let day = Day.init(i, days, dateTestOne, holidays);
     properties.push({
       Name: day.getDayName(),
       altName: day.getDayAltName(),
@@ -31,7 +27,7 @@ function getProperties(dates, jsondata) {
       isHoliday: day.isHoliday(),
       isClosed: day.getHours().length === 0 ? true : false,
       isCurrentDay:
-        new Date(dates[0]).getDate() === day.getDateObject().getDate()
+        new Date(dateTestOne).getDate() === day.getDateObject().getDate()
           ? true
           : false,
       Hours: day.getHours(),
@@ -41,14 +37,14 @@ function getProperties(dates, jsondata) {
   return properties;
 }
 
-let dates = createArrayOfDatesShifted();
-let day = Day.init(0, days, dates[0], holidays);
-let props = getProperties(dates, jsondata);
+//let dates = createArrayOfDatesShifted();
+let day = Day.init(0, days, dateTestOne, holidays);
+let props = getProperties(dateTestOne, jsondata);
 export const buisnessHoursTestObj = [
   {
     description: "Baisc Test",
     dateObj: new Date(
-      dates[0].toLocaleString("en-US", {
+      dateTestOne.toLocaleString("en-US", {
         timeZone: "America/New_York"
       })
     ),
@@ -58,6 +54,6 @@ export const buisnessHoursTestObj = [
     indexCD: 0,
     holidayName: "",
     isHoliday: props[0].isHoliday,
-    isOpen: day.isOpen(dates[0])
+    isOpen: day.isOpen(dateTestOne)
   }
 ];
